438089d6f6e30c01fd16a5817d4802cf124f94c3
ale24cocco <a.cocco76@studenti.unica.it>
2025-05-23 12:42:07 +0200

    Modificata la configurazione docker e docker compose di strapi

--- backend/toy-ecommerce-strapi/config/plugins.ts
+++ backend/toy-ecommerce-strapi/config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+import crypto from "crypto";
+
+export default ({ env }: { env: (key: string) => string }) => ({
+    "users-permissions": {
+        config: {
+            jwtSecret:
+                env("JWT_SECRET") || crypto.randomBytes(16).toString("base64"),
+        },
+    },
+});
--- backend/toy-ecommerce-strapi/config/server.ts
+++ backend/toy-ecommerce-strapi/config/server.ts
@@ -1,7 +1,13 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    app: {
+        keys: env.array("APP_KEYS"),
+    },
+    settings: {
+        cors: {
+            origin: ["http://localhost:3000"],
+            credentials: true,
+        },
+    },
 });
